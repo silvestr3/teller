c36c0fe1a93f14e71c61377dea8bd15d15f2298e
silvestr3 <fellipe.silvestre@outlook.com>
2025-10-06 21:31:01 -0300

    chore(front): 🔧 Update apiClient to include fetch credentials

--- packages/web/src/lib/api-client.ts
+++ packages/web/src/lib/api-client.ts
@@ -2,4 +2,8 @@ import type { App } from "@teller/api";
 import { treaty } from "@elysiajs/eden";
 import { env } from "@/env";
 
-export const apiClient = treaty<App>(env.VITE_API_URL);
+export const apiClient = treaty<App>(env.VITE_API_URL, {
+	fetch: {
+		credentials: "include",
+	},
+});
