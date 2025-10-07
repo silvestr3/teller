077f89038c8185ad869a767daaf4986079e01944
silvestr3 <fellipe.silvestre@outlook.com>
2025-10-07 20:24:57 -0300

    chore(api): 🔧 Update api export so that web package can properly resolve types

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,18 +1,19 @@
 import { Elysia } from "elysia";
-import { env } from "./env";
-import { betterAuthPlugin } from "./http/plugins/better-auth";
-import { corsPlugin } from "./http/plugins/cors";
-import { openapiPlugin } from "./http/plugins/open-api";
-import { incomesModule } from "./modules/incomes";
+import { env } from "@/env";
+import { betterAuthPlugin } from "@/http/plugins/better-auth";
+import { corsPlugin } from "@/http/plugins/cors";
+import { openapiPlugin } from "@/http/plugins/open-api";
+import { incomesModule } from "@/modules/incomes";
 
-export const app = new Elysia()
+const app = new Elysia()
 	.use(openapiPlugin)
 	.use(corsPlugin)
 	.use(betterAuthPlugin)
-	.use(incomesModule)
-	.listen(env.PORT);
+	.use(incomesModule);
 
 export type App = typeof app;
+
+app.listen(env.PORT);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
