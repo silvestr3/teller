53f0ec7529cdeedf39d66605092f7589a192f19a
silvestr3 <fellipe.silvestre@outlook.com>
2025-10-07 19:49:01 -0300

    chore(api): 🔧 Integrate incomes module into the Elysia app

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,11 +3,13 @@ import { env } from "./env";
 import { betterAuthPlugin } from "./http/plugins/better-auth";
 import { corsPlugin } from "./http/plugins/cors";
 import { openapiPlugin } from "./http/plugins/open-api";
+import { incomesModule } from "./modules/incomes";
 
 export const app = new Elysia()
 	.use(openapiPlugin)
 	.use(corsPlugin)
 	.use(betterAuthPlugin)
+	.use(incomesModule)
 	.listen(env.PORT);
 
 export type App = typeof app;
